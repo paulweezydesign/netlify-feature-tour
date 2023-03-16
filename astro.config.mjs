468fd399051a0b7818add7b400c36cdc5572d265
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2023-03-16 18:39:41 +0000

    added framworks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind"; // https://astro.build/config
 
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
-  integrations: [
-    preact(),
-    sitemap({
-      canonicalURL: 'https://feature-tour.netlify.app'
-    })
-  ],
+  integrations: [preact(), sitemap({
+    canonicalURL: 'https://feature-tour.netlify.app'
+  }), tailwind(), svelte(), react(), alpinejs()]
 });
